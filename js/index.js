6d8fec8c635dee16ca65ca289ffea8588aff25b3
i_Cal <mcrunner831@gmail.com>
2023-01-08 00:30:16 -0500

    Fixed active link - Autosave Interval added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 $(function() {
     console.log('Hello, world!');
 
-    // Important constants
-    const MAINCURRENCYABBR = "u";
+    // Important constants/variables
+    const MAIN_CURRENCY_ABBR = "u";
+    const MIN_AUTOSAVE_INTERVAL = 1000;
+    const MAX_AUTOSAVE_INTERVAL = 30000;
+    const DEF_AUTOSAVE_INTERVAL = 15000;
+
+    var autosaveTimer;
 
     // Attempt to load save data
     saveCookie = getCookie("save");
@@ -10,7 +15,8 @@ $(function() {
     if(saveCookie == "") {
         save = {
             mainCurrency: 0,
-            autoSaveEnabled: true
+            autoSaveEnabled: true,
+            autoSaveInterval: DEF_AUTOSAVE_INTERVAL
         };
     } else {
         save = JSON.parse(saveCookie);
@@ -22,22 +28,29 @@ $(function() {
     // Auto-save
     // Autosave every 15 seconds - save to cookies
     // Set interval to 15 seconds (15000 milliseconds)
-    var interval = 15000;
+    if(save.autoSaveInterval === undefined || 
+        save.autoSaveInterval < MIN_AUTOSAVE_INTERVAL || 
+        save.autoSaveInterval > MAX_AUTOSAVE_INTERVAL) {
+        save.autoSaveInterval = 15000;
+    }
 
-    // Call the function every 15 seconds
-    setInterval(function() {
-        if(save.autoSaveEnabled) {
+    if(save.autoSaveEnabled) {
+        // Call the function every 15 seconds
+        autosaveTimer = setInterval(function() {
+            saveGameData(true);
+        }, save.autoSaveInterval);
+    }
+
+    function saveGameData(isAutoSave) {
+        if(isAutoSave) {
             console.log("Autosaving...");
-
-            setCookie("save", JSON.stringify(save), 1);
-    
-            console.log("Autosaved finished. Data:");
-            console.log(save);
         }
-        else {
-            console.log("Autosave is not enabled.");
-        }
-    }, interval);
+        
+        setCookie("save", JSON.stringify(save), 1);
+        
+        console.log("Save finished. Data:");
+        console.log(save);
+    }
 
     function setCookie(name, value, expiration) {
         var date = new Date();
@@ -83,19 +96,24 @@ $(function() {
     var devToDoDiv = $("#devToDoDiv");
 
     homeLink.on("click", function() {
+        // Remove active class from all li in nav
         switchToPage(HOME);
+        $(this).parent().addClass("active");
     });
 
     shopLink.on("click", function() {
         switchToPage(SHOP);
+        $(this).parent().addClass("active");
     });
 
     settingsLink.on("click", function() {
         switchToPage(SETTINGS);
+        $(this).parent().addClass("active");
     });
 
     devToDoLink.on("click", function() {
         switchToPage(DEVTODO);
+        $(this).parent().addClass("active");
     });
 
     function switchToPage(target) {
@@ -103,6 +121,9 @@ $(function() {
             if(currentPage != target) {
                 // Lock page switch
                 pageSwitchLocked = true;
+
+                // Remove active classes from all links
+                $("nav").find("li").removeClass("active");
 
                 // Hide the current page
                 switch(currentPage) {
@@ -141,6 +162,7 @@ $(function() {
                     switch(target) {
                         case HOME:
                             console.log('Switching to Home page.');
+                            homeDiv.parent
                             homeDiv.slideToggle();
                             break;
                         case SHOP:
@@ -167,23 +189,65 @@ $(function() {
         }
     }
 
-    // Settings page code
+    /* Settings code */
+    // Autosave Enabled
     var settingAutosaveEnabled = $("#settingAutosaveEnabled");
     settingAutosaveEnabled.on("click", function() {
         console.log("Autosave enabled: " + settingAutosaveEnabled.prop("checked"));
         save.autoSaveEnabled = settingAutosaveEnabled.prop("checked");
+        settingAutosaveInterval.prop("disabled", !save.autoSaveEnabled);
+        
+        if(save.autoSaveEnabled) {
+            autosaveTimer = setInterval(() => {
+                saveGameData();
+            }, save.autoSaveInterval);
+        } else {
+            clearInterval(autosaveTimer);
+        }
+
+        saveGameData(); // To save the new setting
     });
 
-    // Main currency button code
+    // Autosave Interval
+    var settingAutosaveInterval = $("#settingAutosaveInterval");
+    var settingAutosaveIntervalCurrentValue = $("#settingAutosaveIntervalCurrentValue");
+
+    settingAutosaveInterval.prop("disabled", !save.autoSaveEnabled);
+    settingAutosaveInterval.val(save.autoSaveInterval / 1000);
+    settingAutosaveIntervalCurrentValue.text((save.autoSaveInterval / 1000) + " second" + (save.autoSaveInterval / 1000 != 1 ? "s" : ""));
+
+    settingAutosaveInterval.on("input", function() {
+        var value = $(this).val();
+        settingAutosaveIntervalCurrentValue.text(value + " second" + (value != 1 ? "s" : ""));
+    });
+
+    settingAutosaveInterval.on("change", function() {
+        var value = $(this).val();
+        
+        // Update save data
+        save.autoSaveInterval = value * 1000;
+
+        // Clear old interval
+        clearInterval(autosaveTimer);
+
+        // Set new interval
+        autosaveTimer = setInterval(() => {
+            saveGameData(true);
+        }, save.autoSaveInterval);
+
+        console.log("Autosave interval has been changed to: " + value + " second" + (value != 1 ? "s" : ""));
+    });
+
+    /* Main Currency Button Code */
     var lblMainCurrencyText = $("#navMainCurrency");
     var btnClickMe = $("#btnClickMe");
 
-    lblMainCurrencyText.text(save.mainCurrency + MAINCURRENCYABBR);
+    lblMainCurrencyText.text(save.mainCurrency + MAIN_CURRENCY_ABBR);
 
     btnClickMe.on("click", addMainCurrency);
 
     function addMainCurrency() {
         save.mainCurrency += 1;
-        lblMainCurrencyText.text(save.mainCurrency + MAINCURRENCYABBR);
+        lblMainCurrencyText.text(save.mainCurrency + MAIN_CURRENCY_ABBR);
     }
 })
